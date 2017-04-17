f0b3f2dd5b405974e9892a2171b0aef27b5338dc
Brandon <railsdev14@gmail.com>
2017-04-17 16:08:59 -0400

    Added functionality to add and delete blog posts

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts-index';
+import PostsNew from './components/posts-new';
+import PostsShow from './components/posts-show';
 
 export default (
   <Route path="/" component={App} >
     <IndexRoute component={PostsIndex} />
-    {/*<Route path="" component={} />
-    <Route path="" component={} />*/}
+    <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
